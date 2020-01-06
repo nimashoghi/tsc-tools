f48b890bcd329981014b9f0f48070629fbd281dc
Nima Shoghi <nimashoghi@gmail.com>
2020-01-06 03:13:16 +0000

    Added initial version

---
+++ src/tsc-build-command.ts
@@ -0,0 +1,11 @@
+import {command} from "execa"
+import {packages} from "./util"
+
+const main = async ([, , ...args]: string[]) => {
+    const pckgs = (await packages()).map(({location}) => location).join(" ")
+    const commandToRun = `yarn tsc --build ${args.join(" ")} ${pckgs}`
+    console.log(commandToRun)
+    command(commandToRun)
+}
+
+main(process.argv).catch(console.error)
